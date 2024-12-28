f74763f80cec23926199edd74db45b28c13e6ce0
Caique <asapcaique7@gmail.com>
2024-12-28 11:52:20 -0300

    SimplePigLatin with isLetter conditional

--- projects/SimplePigLatin/index.js
+++ projects/SimplePigLatin/index.js
@@ -2,7 +2,8 @@ function pigIt(str){
     let words = str.split(' ');
     let result = [];
       for (let i = 0; i < words.length; i++) {
-        result.push(words[i].slice(1) + words[i][0] + 'ay');
+        result.push(words[i].match(/[a-z]/i) ? 
+        words[i].slice(1) + words[i][0] + 'ay' : words[i]);
       }
     
     return result.join(' ');
